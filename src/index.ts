8d052cbb544e923c1ba9af2d6c3dd01a32182ffb
Halil Savaşcı <halilsavascihs@gmail.com>
2020-10-09 20:34:06 +0300

    main service client auth fix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-import { QueryBuilder } from '../../services/ProductService2/src/search/queryBuilder'
-import { CategoryTree, Filter, Product, SearchResponse, ServiceResponse, List, BulkUpdateItem, StockOperationResult, SingleMerchantProductStock } from '../../services/ProductService2/src/search/models'
+import {QueryBuilder} from '../../services/ProductService2/src/search/queryBuilder'
+import {
+    CategoryTree,
+    Filter,
+    Product,
+    SearchResponse,
+    ServiceResponse,
+    List,
+    BulkUpdateItem,
+    StockOperationResult,
+    SingleMerchantProductStock
+} from '../../services/ProductService2/src/search/models'
 
 import axios from 'axios'
+import {verify} from "jsonwebtoken";
 
-const SERVICE_URL = 'https://rbs.rettermobile.com'
-const SERVICE_URL_TEST = 'https://rbsmaintest.rettermobile.com'
+const SERVICE_URL = process.env.SERVICE_URL || 'https://rbs.rettermobile.com'
+const SERVICE_URL_TEST = process.env.SERVICE_URL_TEST || 'https://rbsmaintest.rettermobile.com'
 const AGGS_ENDPOINT = '/ProductService2/aggs'
 const SEARCH_ENDPOINT = '/ProductService2/search'
 
@@ -14,24 +25,25 @@ interface RBSConfiguration {
     serviceUrl?: string
     enableLogs?: boolean
     testEnv?: boolean
+    endpoint: "server" | "client"
 }
 
-enum SortOrder {
+export enum SortOrder {
     ASC, DESC
 }
 
 interface SearchInput {
     userId?: string
     searchTerm?: string
-    categoryId: string
-    culture: string
+    categoryId?: string
+    culture?: string
     filters?: Array<Filter>
-    aggs: boolean,
+    aggs?: boolean,
     from?: number,
     size?: number,
     sortAttribute?: string,
     sortOrder?: SortOrder,
-    inStock: boolean
+    inStock?: boolean
 }
 
 interface StockOperationStockItem {
@@ -44,6 +56,24 @@ interface StockOperation {
     stocks: Array<StockOperationStockItem>
 }
 
+type RbsJwtToken = string
+
+interface CustomToken {
+    customToken: RbsJwtToken
+}
+
+interface ClientAuthenticateResponse {
+    accessToken: RbsJwtToken;
+    refreshToken: RbsJwtToken
+}
+
+export interface RbsTokenPayload {
+    projectId: string;
+    userId: string;
+    iat: number;
+    exp: number;
+}
+
 
 /**
  * RBSClient
@@ -54,7 +84,9 @@ export default class RBSClient {
 
     constructor(config: RBSConfiguration) {
         this.config = config
-        if (!this.config.serviceUrl) this.config.serviceUrl = config.testEnv ? SERVICE_URL_TEST : SERVICE_URL
+        if (!this.config.serviceUrl) {
+            this.config.serviceUrl = config.testEnv ? SERVICE_URL_TEST + '/' + config.endpoint : SERVICE_URL + '/' + config.endpoint
+        }
 
         if (this.config.enableLogs) {
             axios.interceptors.request.use(request => {
@@ -62,21 +94,26 @@ export default class RBSClient {
                 return request
             })
             axios.interceptors.response.use(response => {
-                console.log(JSON.stringify(response, null, 4))
+                console.log(response)
+                //console.log(JSON.stringify(response, null, 4))
                 return response
             })
         }
     }
 
-    private addApiKey = (url:string) : string => {
-        if(!this.config.apiKey) return url
-        if(!url.includes('?')) url = url + '?'
+    getRbsTokenPayload(token: RbsJwtToken): RbsTokenPayload {
+        return <RbsTokenPayload>JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString('utf-8'))
+    }
+
+    private addApiKey = (url: string): string => {
+        if (!this.config.apiKey) return url
+        if (!url.includes('?')) url = url + '?'
         return url + '&auth=' + this.config.apiKey
     }
 
-    public executeStockOperation = (operations:Array<StockOperation>, decrease: boolean = false, simulated:boolean = false) : Promise<ServiceResponse<StockOperationResult>> => {
+    public executeStockOperation = (operations: Array<StockOperation>, decrease: boolean = false, simulated: boolean = false): Promise<ServiceResponse<StockOperationResult>> => {
 
-        if(!this.config.merchantId) throw new Error('MerchantId should be set in constructor.')
+        if (!this.config.merchantId) throw new Error('MerchantId should be set in constructor.')
 
         return new Promise<ServiceResponse<StockOperationResult>>((resolve, reject) => {
 
@@ -99,7 +136,7 @@ export default class RBSClient {
                     reject(new Error(response.data.message))
                 }
             }).catch(error => {
-                if(error.response.data && error.response.data.message) {
+                if (error.response.data && error.response.data.message) {
                     reject(new Error(error.response.data.message))
                 } else {
 
@@ -109,7 +146,17 @@ export default class RBSClient {
         })
     }
 
-    public search = (input: SearchInput = { filters: [], aggs: false, categoryId: '', culture: 'en_US', from: 0, size: 20, inStock: false, sortAttribute: 'price', sortOrder: SortOrder.DESC }): Promise<SearchResponse> => {
+    public search = (input: SearchInput = {
+        filters: [],
+        aggs: false,
+        categoryId: '',
+        culture: 'en_US',
+        from: 0,
+        size: 20,
+        inStock: false,
+        sortAttribute: 'price',
+        sortOrder: SortOrder.DESC
+    }): Promise<SearchResponse> => {
         if (!input.userId) throw new Error('UserId is missing')
 
         return new Promise<SearchResponse>((resolve, reject) => {
@@ -132,8 +179,7 @@ export default class RBSClient {
                 url += '&searchTerm=' + input.searchTerm
             }
             axios.get(this.addApiKey(url), {
-                headers: {
-                },
+                headers: {},
             }).then(response => {
                 resolve(response.data)
             }).catch(error => {
@@ -142,7 +188,7 @@ export default class RBSClient {
         })
     }
 
-    public updateMerchantData = (items:Array<BulkUpdateItem>): Promise<ServiceResponse<Boolean>> => {
+    public updateMerchantData = (items: Array<BulkUpdateItem>): Promise<ServiceResponse<Boolean>> => {
         return new Promise<ServiceResponse<Boolean>>((resolve, reject) => {
             let url = `${this.config.serviceUrl!}/ProductService2/updateMerchantData`
             axios.post(this.addApiKey(url), items).then(response => {
@@ -158,15 +204,12 @@ export default class RBSClient {
     }
 
 
-
     public getProduct = (productId: string, culture: string = 'en_US', merchantId?: string): Promise<ServiceResponse<Product>> => {
         return new Promise<ServiceResponse<Product>>((resolve, reject) => {
             let url = `${this.config.serviceUrl!}/ProductService2/getProduct?productId=${productId}&culture=${culture}`
             if (merchantId) url += `&merchantId=${merchantId}`
             axios.get(this.addApiKey(url), {
-                headers: {
-
-                }
+                headers: {}
             }).then(response => {
 
                 if (response.data.success) {
@@ -222,9 +265,7 @@ export default class RBSClient {
             let productIdListStr = productIds.join('|')
             let url = `${this.config.serviceUrl!}/ProductService2/getMultipleProducts?productIds=${productIdListStr}&culture=${culture}`
             axios.get(this.addApiKey(url), {
-                headers: {
-
-                }
+                headers: {}
             }).then(response => {
 
                 if (response.data.success) {
@@ -243,9 +284,7 @@ export default class RBSClient {
         return new Promise<ServiceResponse<CategoryTree>>((resolve, reject) => {
             let url = `${this.config.serviceUrl!}/ProductService2/getCategories?culture=${culture}`
             axios.get(this.addApiKey(url), {
-                headers: {
-
-                }
+                headers: {}
             }).then(response => {
 
                 if (response.data.success) {
@@ -260,19 +299,72 @@ export default class RBSClient {
         })
     }
 
-    public getListProducts = (listId:string, culture: string = 'en_US', inStock: boolean = false): Promise<ServiceResponse<List>> => {
+    public getListProducts = (listId: string, culture: string = 'en_US', inStock: boolean = false): Promise<ServiceResponse<List>> => {
         return new Promise<ServiceResponse<List>>((resolve, reject) => {
             let url = `${this.config.serviceUrl!}/ProductService2/getList?culture=${culture}&listId=${listId}`
             if (inStock) {
                 url += '&inStock=' + inStock
             }
             axios.get(this.addApiKey(url), {
-                headers: {
-
-                }
+                headers: {}
             }).then(response => {
 
                 if (response.data.success) {
+                    resolve(response.data)
+                } else {
+                    reject(new Error(response.data.message))
+                }
+
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    }
+
+    public generateCustomToken = (userId: string): Promise<CustomToken> => {
+        return new Promise<CustomToken>((resolve, reject) => {
+            let url = `${this.config.serviceUrl!}/MainService/token?userId=${userId}`
+            axios.get(this.addApiKey(url), {
+                headers: {}
+            }).then(response => {
+
+                if (response.status >= 200 && response.status < 400) {
+                    resolve(response.data)
+                } else {
+                    reject(new Error(response.data.message))
+                }
+
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    }
+
+    public clientAuthenticate = (props: CustomToken): Promise<ClientAuthenticateResponse> => {
+        return new Promise<ClientAuthenticateResponse>((resolve, reject) => {
+            let url = `${this.config.serviceUrl!}/MainService/public/authenticate`
+            axios.post(url, props).then(response => {
+
+                if (response.status >= 200 && response.status < 400) {
+                    resolve(response.data)
+                } else {
+                    reject(new Error(response.data.message))
+                }
+
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    }
+
+    public clientRefreshToken = (refreshToken: RbsJwtToken): Promise<ClientAuthenticateResponse> => {
+        return new Promise<ClientAuthenticateResponse>((resolve, reject) => {
+            let url = `${this.config.serviceUrl!}/MainService/public/refresh-token`
+            axios.post(url, {
+                refreshToken
+            }).then(response => {
+
+                if (response.status >= 200 && response.status < 400) {
                     resolve(response.data)
                 } else {
                     reject(new Error(response.data.message))
